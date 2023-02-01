67f6b347e3d5f9306dcaeb671e36d530970c1ee1
yogiear <yogieariana.ftmd18@gmail.com>
2023-02-01 17:45:02 +0700

    Mengubah program random team menjadi program pencarian anggota tim

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ teams = [   ['Eka', 'Ega', 'Eki'],
         ]
 total_team = len(teams)
 
-# print a random name from teams
-print(f'Kelompok yang terpilih hari ini iadalah {choice(teams)}')
+# # print a random name from teams
+# print(f'Kelompok yang terpilih hari ini iadalah {choice(teams)}')
 
-# #  input member name
-# nama = input('Masukkan nama: ')
+#  input member name
+nama = input('Masukkan nama: ')
 
-# #  loop and find matching team
-# for i in range(0,total_team) :
-#     if nama == teams[i][0] or teams[i][1] or teams[i][2] :
-#         print(f'{nama} adalah bagian dari tim {teams[i]}')
-#     else :
-#         print(f'Nama {nama} tidak ada di dalam list team!!')
+#  loop and find matching team
+for i in range(0,total_team) :
+    if nama == teams[i][0] or teams[i][1] or teams[i][2] :
+        print(f'{nama} adalah bagian dari tim {teams[i]}')
+    else :
+        print(f'Nama {nama} tidak ada di dalam list team!!')
 
-# # print('Ega' == teams[i[0]][1] or teams[1][2])
+# print('Ega' == teams[i[0]][1] or teams[1][2])
